c69925644a5a7136887ddcd4a8f93c7043491c6e
al-nf <alankjfung@gmail.com>
2024-12-03 23:55:18 -0800

    yes

---
+++ .config/nvim/init.lua
@@ -0,0 +1,90 @@
+vim.cmd("set modelines=0")
+vim.cmd("set backspace=indent,eol,start")
+vim.cmd("au BufWrite /private/tmp/crontab.* set nowritebackup nobackup")
+vim.cmd("au BufWrite /private/etc/pw.* set nowritebackup nobackup")
+vim.cmd("let skip_defaults_vim=1")
+vim.cmd("filetype on")
+vim.cmd("filetype plugin on")
+vim.cmd("filetype indent on")
+vim.cmd("syntax on")
+vim.cmd("set nu")
+vim.cmd("set rnu")
+vim.cmd("set tabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set expandtab")
+vim.cmd("set softtabstop=-1")
+vim.cmd("set incsearch")
+vim.cmd("set autoindent")
+
+require("config.lazy")
+require('wlsample.bubble')
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+  -- List of parsers to ignore installing (or "all")
+  ignore_install = { "javascript" },
+
+  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+  highlight = {
+    enable = true,
+
+    disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+            return true
+        end
+    end,
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
+
+-- Markdown Preview settings
+vim.g.mkdp_auto_start = 1
+vim.g.mkdp_auto_close = 1
+vim.g.mkdp_refresh_slow = 0
+vim.g.mkdp_command_for_global = 0
+vim.g.mkdp_open_to_the_world = 0
+vim.g.mkdp_open_ip = ''
+vim.g.mkdp_browser = 'firefox'
+vim.g.mkdp_echo_preview_url = 0
+vim.g.mkdp_browserfunc = ''
+vim.g.mkdp_preview_options = {
+    mkit = {},
+    katex = {},
+    uml = {},
+    maid = {},
+    disable_sync_scroll = 0,
+    sync_scroll_type = 'middle',
+    hide_yaml_meta = 1,
+    sequence_diagrams = {},
+    flowchart_diagrams = {},
+    content_editable = false,
+    disable_filename = 0,
+    toc = {}
+}
+vim.g.mkdp_markdown_css = ''
+vim.g.mkdp_highlight_css = ''
+vim.g.mkdp_port = '3333'
+vim.g.mkdp_page_title = '「${name}.md」'
+vim.g.mkdp_images_path = '/home/user/.markdown_images'
+vim.g.mkdp_filetypes = {'markdown'}
+vim.g.mkdp_theme = 'dark'
+vim.g.mkdp_combine_preview = 0
+vim.g.mkdp_combine_preview_auto_refresh = 1
+
---
+++ .config/spotify/Users/31dxzpuzfylyim53q4cv2qe2fhb4-user/frecency.pb
@@ -0,0 +1,49 @@
+
+>
+'spotify:playlist:1SkyadwLGqxXrL4cb7TRbv	û¼Hy@ Ã´…¹
+>
+'spotify:playlist:6DYT5gQKwTAZrvyDGhQR1f	Õ&Æ‚µy@ ãÈÕ¹
+>
+'spotify:playlist:1ECJxRC6dmNTizrWZN9gTJ	.°Þ+ó¼y@ ¹¥žº
+>
+'spotify:playlist:2KE4VlsCKCnRBW28jCVRdY	,_¿f±y@ úÕµ¹
+>
+'spotify:playlist:5rTF8kLCBazD98FY6rRz5x	ñø‚´y@ ­Öµ¹
+>
+'spotify:playlist:45MpJlIubDAqiXOsWy0Svm	‡Ü¹ÅÖ´y@ ‘ƒ»¹
+>
+'spotify:playlist:37i9dQZF1DZ06evO1rNkUo	à½P½µy@ »£Ù¹
+>
+'spotify:playlist:2kDoopYTfC1SDqVwW9PdyF	Í°›ã´y@ ¯‡î¹
+>
+'spotify:playlist:3XA8wMyEnIdt8AbhdfCcbW	øÌqü:‚y@ êÚå¸
+>
+'spotify:playlist:6Mbx4NIM2DeRy0tIfVCHKw	„6«ã´y@ ·‡î¹
+`
+4spotify:user:31dxzpuzfylyim53q4cv2qe2fhb4:collection	ùmüvŒy@ ¿•‹º	å>ÛÈ‚y@ ÐÕî¸
+S
+'spotify:playlist:37i9dQZF1DXbITWG1ZJKYt	›ø£y@ £žº	ýZr¾¼y@ Äžº
+>
+'spotify:playlist:3fkUv4NEf4wL7tbKWTLMbJ	`%²PŸy@ †ßŸº
+>
+'spotify:playlist:7scHYWynTlSaMSIzyJfxde	:—ì\½µy@ °¢Ù¹
+>
+'spotify:playlist:504oDhYZUJ5Suo1yEb840a	ÇDmZE¿y@ É º
+>
+'spotify:playlist:1avQBicfD2eOc7O30NG9J2	~iÐÞÌy@3 ³¦´º
+>
+'spotify:playlist:2J7d739pNWHDiUd4OYljcA	xZé"¸y@ …•©º
+>
+'spotify:playlist:1x58NIsiIblgq7NowA5yCE	C±~‘ŠŸy@ ì²£º
+S
+'spotify:playlist:5vmznq99u46Y63aDExkN66	õ¶ÆÝˆy@ ÷þÐ¹	8×XÖOÍy@8 ¾í´º
+>
+'spotify:playlist:7dVS5xb6ulmTtxHFaVL0l3	–c¥×Àºy@ ³ð³º
+>
+'spotify:playlist:37i9dQZF1DZ06evO2VxlyE	lÿêDy@ Õ“…¹
+>
+'spotify:playlist:5sR0kCCf1StzaUqP0rEpK3	;'ç‘/ºy@ óå©º
+>
+'spotify:playlist:1F5hUxfuXBV2NbOKl9TJfY	R¸V¿…´y@ ­¦û¹
+?
+'spotify:playlist:5SW7lbMOx6RY2DsEr3D1go	>-6ÌêÜy@— ÀÃ³º
